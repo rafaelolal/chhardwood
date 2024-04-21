476c217a3f01a689540f6a351992f7a7bde8ba50
dlewis123 <dslewis1317@gmail.com>
2024-04-21 04:04:02 -0400

    Restyled Gallary

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -19,10 +19,12 @@ export default function Gallery() {
   ];
 
   return (
-    <main>
-      <h1>Gallery</h1>
-      <p>The best way to communicate what we do</p>
-
+    <main >
+      <div className="row py-4">
+      <h6 className="fs-4 text-center text-primary m-0 p-0">Gallery</h6>
+      <h6 className="body-text fs-6 text-center m-0 mb-2">The best way to communicate what we do</h6>
+      <div className="bar"/>
+</div>
       <ImageList images={images} />
     </main>
   );
