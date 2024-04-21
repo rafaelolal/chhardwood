588975b82ff66425c4cf54db73c907e22c48ecc7
dlewis123 <dslewis1317@gmail.com>
2024-04-21 04:00:57 -0400

    Redesigned get in touch page

--- app/message/page.tsx
+++ app/message/page.tsx
@@ -1,55 +1,54 @@
 export default function Message() {
   return (
     <main>
+      <div className="row pt-5">
+      <h6 className="fs-4 text-center text-primary m-0 p-0">Get In Touch</h6>
+      <h6 className="body-text fs-6 text-center m-0 mb-2">Let us know your thoughts</h6>
+     
+</div>
+<div className="row py-4">
+  <div className="col-9 mx-auto">
       <div className="mb-3">
-        <label htmlFor="fullNameInput" className="form-label">
-          Full Name
-        </label>
+
 
         <input
           type="text"
-          className="form-control"
+          className="form-control input-box"
           id="fullNameInput"
-          placeholder="John Doe"
+          placeholder="Full Name"
         />
       </div>
 
       <div className="mb-3">
-        <label htmlFor="emailInput" className="form-label">
-          Email address
-        </label>
+
 
         <input
           type="email"
-          className="form-control"
+          className="form-control input-box"
           id="emailInput"
-          placeholder="name@example.com"
+          placeholder="Email Address"
         />
       </div>
 
       <div className="mb-3">
-        <label htmlFor="subjectInput" className="form-label">
-          Subject
-        </label>
 
         <input
           type="text"
-          className="form-control"
+          className="form-control input-box"
           id="subjectInput"
-          placeholder="Hello!"
+          placeholder="Subject"
         />
       </div>
 
       <div className="mb-3">
-        <label htmlFor="contentInput" className="form-label">
-          Content
-        </label>
-
         <textarea
-          className="form-control"
+          className="form-control input-box"
+          placeholder="Content"
           id="contentInput"
           rows={3}
         ></textarea>
+      </div>
+      </div>
       </div>
     </main>
   );
