1bc4a3da1e0d6d956e12b68776b0f07506a5ac1a
Rafael Almeida <rafaelpbcp@gmail.com>
2024-04-29 14:41:58 +0000

    fixed minor warnings

--- app/ui/image-list.tsx
+++ app/ui/image-list.tsx
@@ -14,7 +14,7 @@ export default function ImageList({ images }: ImageListProps) {
     <div className="row g-3 g-lg-4 row-cols-2 row-cols-sm-3 row-cols-md-4">
       {images.map((image, i) => {
         return (
-          <div className="col">
+          <div className="col" key={i}>
             <div
               className="position-relative card-img"
               style={{ width: "100%", height: "300px" }}
