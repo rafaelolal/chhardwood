e2ddea546e31e568c49f07e96d084cd008816dac
dlewis123 <dslewis1317@gmail.com>
2024-04-28 23:31:55 -0400

    styling services

--- app/ui/service.tsx
+++ app/ui/service.tsx
@@ -17,24 +17,24 @@ type ServiceProps = {
 
 export default function Service({ service }: ServiceProps) {
   return (
-    <div className="col">
-      <div className="card" style={{ width: "18rem" }}>
+    <div className="col-12 col-sm-6 col-lg-3 my-3">
+      <div className="card mx-auto bg-light" style={{ width: "17rem" }}>
         <div
           className="card-img-top position-relative"
-          style={{ height: 100, width: "100%" }}
+          style={{ height: 130, width: "100%" }}
         >
           <Image
             src={service.image.link}
             alt={service.image.alt}
             fill
-            style={{ objectFit: "contain" }}
+            objectFit="cover"
           />
         </div>
 
-        <div className="card-body">
-          <h5 className="card-title">{service.name}</h5>
-          <p className="card-text">{service.description}</p>
-          <Link href={service.link} className="btn btn-primary">
+        <div className="card-body text-center">
+          <h5 className="card-title fs-6">{service.name}</h5>
+          <p className="card-text fs-7">{service.description}</p>
+          <Link href={service.link} className="btn btn-tertiary fs-7">
             More
           </Link>
         </div>
