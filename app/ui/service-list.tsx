12bb9c78a0deef6a208d13dea77f46646544ce2d
dlewis123 <dslewis1317@gmail.com>
2024-04-28 23:31:55 -0400

    styling services

--- app/ui/service-list.tsx
+++ app/ui/service-list.tsx
@@ -18,7 +18,7 @@ type ServiceListProps = {
 
 export default function ServiceList({ services }: ServiceListProps) {
   return (
-    <div className="row py-2 justify-content-evenly">
+    <div className="row py-2 pb-5 justify-content-start">
       {services.map((service, i) => (
         <Service key={i} service={service} />
       ))}
